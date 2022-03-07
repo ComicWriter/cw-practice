3b105698eec3745573ee4674a090ee574711e784
Janice MacKinnon <jrmackinnon@hotmail.com>
2022-03-06 23:12:35 -0800

    Add make-upper-case.js

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,9 @@
+// Description
+// Write a function which converts the input string to uppercase.
+// function makeUpperCase(str) {
+//
+// }
+//
+// My Solution - forgot semi-colon tho
+
+const makeUpperCase = (str) => str.toUpperCase();
